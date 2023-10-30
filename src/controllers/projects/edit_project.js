2a7e4f537df6ccdefc072e12ab118dad7a560c0e
Tolu <tolulopeoduro345@gmail.com>
2023-10-31 00:13:38 +0100

    Added project editing endpoint, to refine later

--- src/controllers/projects/edit_project.js
+++ src/controllers/projects/edit_project.js
@@ -1,13 +1,13 @@
-import { BSON, ObjectId } from "bson";
+import { ObjectId } from "bson";
 
 export default async (req, res, next) => {
 	req.app.locals.client.db("buildr").collection("projects")
 	.updateOne(
-		{_id: req.body.id},
+		{_id: new ObjectId(req.body.id)},
 		{$set : {
 			...req.body.data
 		}},
-		{upsert : true}
+		{upsert : false}
 	).then (() => {
 		res.status(201).json({
 			message : "Success"
